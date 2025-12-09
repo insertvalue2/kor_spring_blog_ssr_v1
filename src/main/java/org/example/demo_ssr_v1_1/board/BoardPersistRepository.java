6da7d8311655436a7868d7b27b9cd7189f566195
insertvalue <insertvalue@naver.com>
2025-12-09 14:48:40 +0900

    게시글 작성 1단계

--- src/main/java/org/example/demo_ssr_v1_1/board/BoardPersistRepository.java
+++ src/main/java/org/example/demo_ssr_v1_1/board/BoardPersistRepository.java
@@ -1,4 +1,25 @@
 package org.example.demo_ssr_v1_1.board;
 
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+// DB -- CRUD
+@RequiredArgsConstructor
+@Repository // IoC
 public class BoardPersistRepository {
+
+    // DI
+    private final EntityManager entityManager;
+
+    @Transactional
+    public Board save(Board board) {
+        // 엔티티 매니저 자동으로 insert 쿼리 만들어 던진다.
+        entityManager.persist(board);
+        return board;
+    }
 }
