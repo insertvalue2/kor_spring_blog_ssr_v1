e3e0db66e50d36a9b0b072b183d1a17bc190c4ea
insertvalue <insertvalue@naver.com>
2025-12-10 11:58:54 +0900

    controller 작업중

--- src/main/java/org/example/demo_ssr_v1_1/board/BoardPersistRepository.java
+++ src/main/java/org/example/demo_ssr_v1_1/board/BoardPersistRepository.java
@@ -32,4 +32,40 @@ public class BoardPersistRepository {
                 .getResultList();
 
     }
+    // 단 한건만 조회 하기
+    public Board findById(Long id) {
+        Board board = entityManager.find(Board.class, id);
+        return board;
+    }
+
+    // 수정 하기
+    @Transactional
+    public Board updateById(Long id, BoardRequest.UpdateDTO reqDTO) {
+        Board board = entityManager.find(Board.class, id);
+        if(board == null) {
+            throw new IllegalArgumentException("수정할 게시글을 찾을 수 없어요");
+        }
+        board.update(reqDTO);
+//        board.setTitle(req.getTitle());
+//        board.setContent(req.getContent());
+//        board.setUsername(req.getUsername());
+        // 더티 체킹
+        // 1. 개발자가 직접 update 쿼리를 작성 안해도 된다
+        // 2. 변경된 필드만 자동으로 Update 가 된다
+        // 3. 영속성 컨텍스트가 엔티티 상태를 자동 관리 한다
+        // 4. 1차 캐시의 엔티티 정보도 자동 갱신
+        return  board;
+    }
+
+
+    // 삭제 하기
+    @Transactional
+    public void deleteById(Long id) {
+        Board board = entityManager.find(Board.class, id);
+        if(board == null) {
+            throw new IllegalArgumentException("삭제할 게시글이 없어요");
+        }
+        entityManager.remove(board);
+    }
+
 }
