7103e244dafea4eefaff299c69d8ff4279a10692
insertvalue <insertvalue@naver.com>
2025-12-17 15:48:25 +0900

    페이지 처리 완료

--- src/main/java/org/example/demo_ssr_v1_1/board/BoardController.java
+++ src/main/java/org/example/demo_ssr_v1_1/board/BoardController.java
@@ -7,11 +7,10 @@ import org.example.demo_ssr_v1_1._core.errors.exception.*;
 import org.example.demo_ssr_v1_1.reply.ReplyResponse;
 import org.example.demo_ssr_v1_1.reply.ReplyService;
 import org.example.demo_ssr_v1_1.user.User;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -61,19 +60,40 @@ public class BoardController {
         return "redirect:/board/list";
     }
 
+    /**
+     * 게시글 목록 페이징 처리 기능 추가
+     * @param model
+     * @return
+     * // 예시:  /board/list?page=1&size=5
+     */
+    @GetMapping({"/board/list", "/"})
+    public String boardList(Model model,
+                                 @RequestParam(defaultValue = "1") int page,
+                                 @RequestParam(defaultValue = "3") int size) {
+
+        int pageIndex = Math.max(0, page - 1);
+        BoardResponse.PageDTO boardPage = boardService.게시글목록조회(pageIndex, size);
+        model.addAttribute("boardPage", boardPage);
+        return "board/list";
+    }
+
 
     /**
+     * TODO - 삭제 예정
      * 게시글 목록 화면 요청
      * @param model
      * @return
      */
-    @GetMapping({"/board/list", "/"})
-    public String boardList(Model model) {
-        List<BoardResponse.ListDTO> boardList = boardService.게시글목록조회();
-        model.addAttribute("boardList", boardList);
+//    @GetMapping({"/board/list", "/"})
+//    public String boardList(Model model) {
+//        List<BoardResponse.ListDTO> boardList = boardService.게시글목록조회();
+//        model.addAttribute("boardList", boardList);
+//
+//        return "board/list";
+//    }
 
-        return "board/list";
-    }
+
+
 
     /**
      * 게시글 작성 화면 요청
